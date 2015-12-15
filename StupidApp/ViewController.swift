a830e80754b0d2c695bf297beb9ecb11b054b8c6
Orton <eort3611@740s-pl-128.local>
2015-12-15 13:09:38 -0700

    DOESNT WORK

--- StupidApp/ViewController.swift
+++ StupidApp/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+            @IBAction func changeColor
+            (sender: UIButton)
+            {
+                
+                let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+                let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+                let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.00)
+               
+                
+                view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+                
+            }
+        
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
